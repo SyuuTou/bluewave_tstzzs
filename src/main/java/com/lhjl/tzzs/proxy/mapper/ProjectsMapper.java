0df54695dd6ace0c4756b1f762992f0f96bd360d
RoyLiuMingyan <liuwang1012926>
2017-09-27 14:34:53 +0800

    产品

--- src/main/java/com/lhjl/tzzs/proxy/mapper/ProjectsMapper.java
+++ src/main/java/com/lhjl/tzzs/proxy/mapper/ProjectsMapper.java
@@ -1,5 +1,6 @@
 package com.lhjl.tzzs.proxy.mapper;
 
+import com.lhjl.tzzs.proxy.dto.SereachDto;
 import com.lhjl.tzzs.proxy.model.Projects;
 import com.lhjl.tzzs.proxy.utils.OwnerMapper;
 import java.util.List;
@@ -12,10 +13,7 @@ import org.apache.ibatis.annotations.Param;
 public interface ProjectsMapper extends OwnerMapper<Projects> {
     List<Projects> maxSerialNumber();
     List<Projects> findProjectByUserId(String userId);
-    List<Map<String, Object>>findProjectByShortName(@Param("shortName") String shortName,@Param("userId") String userId);
-    List<Map<String, Object>>findProjectByShortNameAll(@Param("shortName") String shortName,@Param("userId") String userId);
-    List<Map<String, Object>>findProjectBySview(@Param("type") Integer  type,@Param("segmentation") String segmentation,
-                                                @Param("stage") String stage,@Param("city") String city,@Param("userId") String userId,
-                                                @Param("working_background_desc") String working_background_desc,@Param("educational_background_desc") String educational_background_desc
-    );
+    List<Map<String, Object>> findProjectByShortName(@Param("shortName") String shortName,@Param("userId") String userId);
+    List<Map<String, Object>> findProjectByShortNameAll(@Param("shortName") String shortName,@Param("userId") String userId);
+    List<Map<String, Object>> findProjectBySview(SereachDto sereachDto);
 }
