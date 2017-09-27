b2b97dce772403dc409ce9e1f38ced778193df71
RoyLiuMingyan <liuwang1012926>
2017-09-27 17:45:32 +0800

    搜索结果

---
+++ src/main/java/com/lhjl/tzzs/proxy/dto/ShuruDto.java
@@ -0,0 +1,19 @@
+package com.lhjl.tzzs.proxy.dto;
+
+public class ShuruDto {
+private String userId;
+private String shortName;
+public String getUserId() {
+	return userId;
+}
+public void setUserId(String userId) {
+	this.userId = userId;
+}
+public String getShortName() {
+	return shortName;
+}
+public void setShortName(String shortName) {
+	this.shortName = shortName;
+}
+
+}
