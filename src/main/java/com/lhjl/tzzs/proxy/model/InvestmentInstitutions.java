a538c7c11ba38db2d822f057f2a984cd8cd328a5
YangYangyy <670052206@qq.com>
2017-09-28 22:39:36 +0800

    提交机构表信息更新

--- src/main/java/com/lhjl/tzzs/proxy/model/InvestmentInstitutions.java
+++ src/main/java/com/lhjl/tzzs/proxy/model/InvestmentInstitutions.java
@@ -30,8 +30,44 @@ public class InvestmentInstitutions {
     /**
      * 机构类型：1: 50机构，0: 非50机构
      */
-    @Column(name = "`type`")
     private Integer type;
+
+    /**
+     * logoURl
+     */
+    private String logo;
+
+    /**
+     * 案例网址
+     */
+    @Column(name = "case_url")
+    private String caseUrl;
+
+    /**
+     * 一句话介绍
+     */
+    @Column(name = "kenel_case")
+    private String kenelCase;
+
+    /**
+     * 简介
+     */
+    private String comment;
+
+    /**
+     * 所在城市
+     */
+    private String city;
+
+    /**
+     * 投资阶段
+     */
+    private String stage;
+
+    /**
+     * 客户代表
+     */
+    private String representative;
 
     /**
      * @return ID
@@ -117,5 +153,131 @@ public class InvestmentInstitutions {
      */
     public void setType(Integer type) {
         this.type = type;
+    }
+
+    /**
+     * 获取logoURl
+     *
+     * @return logo - logoURl
+     */
+    public String getLogo() {
+        return logo;
+    }
+
+    /**
+     * 设置logoURl
+     *
+     * @param logo logoURl
+     */
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+    /**
+     * 获取案例网址
+     *
+     * @return case_url - 案例网址
+     */
+    public String getCaseUrl() {
+        return caseUrl;
+    }
+
+    /**
+     * 设置案例网址
+     *
+     * @param caseUrl 案例网址
+     */
+    public void setCaseUrl(String caseUrl) {
+        this.caseUrl = caseUrl;
+    }
+
+    /**
+     * 获取一句话介绍
+     *
+     * @return kenel_case - 一句话介绍
+     */
+    public String getKenelCase() {
+        return kenelCase;
+    }
+
+    /**
+     * 设置一句话介绍
+     *
+     * @param kenelCase 一句话介绍
+     */
+    public void setKenelCase(String kenelCase) {
+        this.kenelCase = kenelCase;
+    }
+
+    /**
+     * 获取简介
+     *
+     * @return comment - 简介
+     */
+    public String getComment() {
+        return comment;
+    }
+
+    /**
+     * 设置简介
+     *
+     * @param comment 简介
+     */
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    /**
+     * 获取所在城市
+     *
+     * @return city - 所在城市
+     */
+    public String getCity() {
+        return city;
+    }
+
+    /**
+     * 设置所在城市
+     *
+     * @param city 所在城市
+     */
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    /**
+     * 获取投资阶段
+     *
+     * @return stage - 投资阶段
+     */
+    public String getStage() {
+        return stage;
+    }
+
+    /**
+     * 设置投资阶段
+     *
+     * @param stage 投资阶段
+     */
+    public void setStage(String stage) {
+        this.stage = stage;
+    }
+
+    /**
+     * 获取客户代表
+     *
+     * @return representative - 客户代表
+     */
+    public String getRepresentative() {
+        return representative;
+    }
+
+    /**
+     * 设置客户代表
+     *
+     * @param representative 客户代表
+     */
+    public void setRepresentative(String representative) {
+        this.representative = representative;
     }
 }
