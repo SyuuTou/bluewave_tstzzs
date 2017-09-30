0b314331df6de2274a938f7ad0f6ff42a735ff23
RoyLiuMingyan <liuwang1012926>
2017-09-30 14:07:55 +0800

    约谈提交自动关注s

--- src/main/java/com/lhjl/tzzs/proxy/service/InterviewService.java
+++ src/main/java/com/lhjl/tzzs/proxy/service/InterviewService.java
@@ -1,5 +1,8 @@
 package com.lhjl.tzzs.proxy.service;
 
+import org.apache.ibatis.annotations.Param;
+
+import com.lhjl.tzzs.proxy.dto.CommonDto;
 import com.lhjl.tzzs.proxy.model.Interview;
 
 /**
@@ -13,5 +16,5 @@ public interface InterviewService {
      * 插入约谈记录
      */
     void insertInterview(Interview interview);
-
+    CommonDto<String> updateFollowStatus1(@Param("yn") Integer yn,@Param("projectId")Integer projectId,@Param("userId")String userId);
 }
