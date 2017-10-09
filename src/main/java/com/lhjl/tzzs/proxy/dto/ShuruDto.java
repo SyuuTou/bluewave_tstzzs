fb47f8aa324af7bd303af570a0f11ba71522bb58
RoyLiuMingyan <liumingyan@idatavc.com>
2017-10-09 13:16:00 +0800

    筛选与搜索分页sDto

--- src/main/java/com/lhjl/tzzs/proxy/dto/ShuruDto.java
+++ src/main/java/com/lhjl/tzzs/proxy/dto/ShuruDto.java
@@ -1,19 +1,34 @@
 package com.lhjl.tzzs.proxy.dto;
 
 public class ShuruDto {
-private String userId;
-private String shortName;
-public String getUserId() {
-	return userId;
-}
-public void setUserId(String userId) {
-	this.userId = userId;
-}
-public String getShortName() {
-	return shortName;
-}
-public void setShortName(String shortName) {
-	this.shortName = shortName;
-}
+	private String userId;
+	private String shortName;
+	private String size;
+	private String from;
+
+	public String getSize() {
+		return size;
+	}
+	public void setSize(String size) {
+		this.size = size;
+	}
+	public String getFrom() {
+		return from;
+	}
+	public void setFrom(String from) {
+		this.from = from;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getShortName() {
+		return shortName;
+	}
+	public void setShortName(String shortName) {
+		this.shortName = shortName;
+	}
 
 }
