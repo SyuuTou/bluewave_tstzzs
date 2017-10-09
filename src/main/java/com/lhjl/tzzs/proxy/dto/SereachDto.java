f169cc6341626f75f62f6b9d39353c60c77605a6
RoyLiuMingyan <liumingyan@idatavc.com>
2017-10-09 13:15:41 +0800

    筛选与搜索分页seDto

--- src/main/java/com/lhjl/tzzs/proxy/dto/SereachDto.java
+++ src/main/java/com/lhjl/tzzs/proxy/dto/SereachDto.java
@@ -1,56 +1,72 @@
 package com.lhjl.tzzs.proxy.dto;
 
 public class SereachDto {
-private String userId;
-private String  investment_institutions_type;
-private String segmentation;
-private String stage;
-private String city;
-private String educational_background_desc;
-private String working_background_desc;
+	private String userId;
+	private String  investment_institutions_type;
+	private String segmentation;
+	private String stage;
+	private String city;
+	private String educational_background_desc;
+	private String working_background_desc;
+	private String size;
+	private String from;
 
-public String getUserId() {
-	return userId;
-}
-public void setUserId(String userId) {
-	this.userId = userId;
-}
+
+
+	public String getSize() {
+		return size;
+	}
+	public void setSize(String size) {
+		this.size = size;
+	}
+	public String getFrom() {
+		return from;
+	}
+	public void setFrom(String from) {
+		this.from = from;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
 	public String getInvestment_institutions_type() {
 		return investment_institutions_type;
 	}
 	public void setInvestment_institutions_type(String investment_institutions_type) {
 		this.investment_institutions_type = investment_institutions_type;
 	}
-public String getSegmentation() {
-	return segmentation;
-}
-public void setSegmentation(String segmentation) {
-	this.segmentation = segmentation;
-}
-public String getStage() {
-	return stage;
-}
-public void setStage(String stage) {
-	this.stage = stage;
-}
-public String getCity() {
-	return city;
-}
-public void setCity(String city) {
-	this.city = city;
-}
-public String getEducational_background_desc() {
-	return educational_background_desc;
-}
-public void setEducational_background_desc(String educational_background_desc) {
-	this.educational_background_desc = educational_background_desc;
-}
-public String getWorking_background_desc() {
-	return working_background_desc;
-}
-public void setWorking_background_desc(String working_background_desc) {
-	this.working_background_desc = working_background_desc;
-}
- 
+	public String getSegmentation() {
+		return segmentation;
+	}
+	public void setSegmentation(String segmentation) {
+		this.segmentation = segmentation;
+	}
+	public String getStage() {
+		return stage;
+	}
+	public void setStage(String stage) {
+		this.stage = stage;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getEducational_background_desc() {
+		return educational_background_desc;
+	}
+	public void setEducational_background_desc(String educational_background_desc) {
+		this.educational_background_desc = educational_background_desc;
+	}
+	public String getWorking_background_desc() {
+		return working_background_desc;
+	}
+	public void setWorking_background_desc(String working_background_desc) {
+		this.working_background_desc = working_background_desc;
+	}
+
 
 }
