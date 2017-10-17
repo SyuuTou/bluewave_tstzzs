4e322a2759d4603f5925ce692eed66217504c885
RoyLiuMingyan <liumingyan@idatavc.com>
2017-10-17 20:24:40 +0800

    增量mp

--- src/main/java/com/lhjl/tzzs/proxy/mapper/UsersMapper.java
+++ src/main/java/com/lhjl/tzzs/proxy/mapper/UsersMapper.java
@@ -1,7 +1,15 @@
 package com.lhjl.tzzs.proxy.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
 import com.lhjl.tzzs.proxy.model.Users;
 import com.lhjl.tzzs.proxy.utils.OwnerMapper;
-
+@Mapper
 public interface UsersMapper extends OwnerMapper<Users> {
+	  Integer findByUuid(@Param("uuids") String uuids);
+	  Integer findByUserid(@Param("userId") Integer userId);
+	  Float findByBei(@Param("leId") Integer leId);
+	  Integer findByJinE(@Param("skey") String skey);
+	 
 }
