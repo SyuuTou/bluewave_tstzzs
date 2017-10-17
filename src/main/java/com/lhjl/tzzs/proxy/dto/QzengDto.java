d808b4919ebfc43f90a7619667399052cd485aee
RoyLiuMingyan <liumingyan@idatavc.com>
2017-10-17 20:22:58 +0800

    其他金额的dto

---
+++ src/main/java/com/lhjl/tzzs/proxy/dto/QzengDto.java
@@ -0,0 +1,24 @@
+package com.lhjl.tzzs.proxy.dto;
+
+public class QzengDto {
+  private Integer qj;
+  private String  uuids;
+  
+
+public String getUuids() {
+	return uuids;
+}
+
+public void setUuids(String uuids) {
+	this.uuids = uuids;
+}
+
+public Integer getQj() {
+	return qj;
+}
+
+public void setQj(Integer qj) {
+	this.qj = qj;
+}
+  
+}
