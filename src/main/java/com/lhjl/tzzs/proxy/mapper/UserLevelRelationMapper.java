a4bd16192f8bd6c3b54a824a91c9967e20483980
RoyLiuMingyan <liumingyan@idatavc.com>
2017-10-17 20:24:20 +0800

    余额mp

--- src/main/java/com/lhjl/tzzs/proxy/mapper/UserLevelRelationMapper.java
+++ src/main/java/com/lhjl/tzzs/proxy/mapper/UserLevelRelationMapper.java
@@ -1,7 +1,13 @@
 package com.lhjl.tzzs.proxy.mapper;
 
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Param;
+
 import com.lhjl.tzzs.proxy.model.UserLevelRelation;
 import com.lhjl.tzzs.proxy.utils.OwnerMapper;
 
 public interface UserLevelRelationMapper extends OwnerMapper<UserLevelRelation> {
+	List<Map<String, Object>> findByMing(@Param("userId") Integer userId, @Param("beginNum") Integer beginNum, @Param("pageSize") Integer pageSize);
 }
