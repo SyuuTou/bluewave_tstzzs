0b384264cc6734c4bdc7132ff894cdca2210c5d8
RoyLiuMingyan <liumingyan@idatavc.com>
2017-10-17 20:23:46 +0800

    增数量dto

---
+++ src/main/java/com/lhjl/tzzs/proxy/dto/ZengDto.java
@@ -0,0 +1,20 @@
+package com.lhjl.tzzs.proxy.dto;
+
+public class ZengDto {
+	private String uuids;
+	private String sKey;
+	public String getUuids() {
+		return uuids;
+	}
+	public void setUuids(String uuids) {
+		this.uuids = uuids;
+	}
+	public String getsKey() {
+		return sKey;
+	}
+	public void setsKey(String sKey) {
+		this.sKey = sKey;
+	}
+	
+
+}
