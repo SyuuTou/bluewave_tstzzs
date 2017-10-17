551f7ee16d66eac9f838aaf478664c41b1f29fe9
RoyLiuMingyan <liumingyan@idatavc.com>
2017-10-17 20:24:03 +0800

    增数量mp

--- src/main/java/com/lhjl/tzzs/proxy/mapper/UserIntegralsMapper.java
+++ src/main/java/com/lhjl/tzzs/proxy/mapper/UserIntegralsMapper.java
@@ -1,7 +1,17 @@
 package com.lhjl.tzzs.proxy.mapper;
 
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
 import com.lhjl.tzzs.proxy.model.UserIntegrals;
 import com.lhjl.tzzs.proxy.utils.OwnerMapper;
-
+@Mapper
 public interface UserIntegralsMapper extends OwnerMapper<UserIntegrals> {
-}
+	
+	Integer findIntegralsZ(@Param("userId") Integer userId);
+	Integer findIntegralsX(@Param("userId") Integer userId);
+	
+	}
