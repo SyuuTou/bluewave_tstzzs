b5c401baa9fa1a673a84cb9cd66ff4cf03b69be7
zhaolz <colin0702@163.com>
2017-10-20 14:51:01 +0800

    重提Mapper

---
+++ src/main/java/com/lhjl/tzzs/proxy/mapper/UserChooseRecordMapper.java
@@ -0,0 +1,7 @@
+package com.lhjl.tzzs.proxy.mapper;
+
+import com.lhjl.tzzs.proxy.model.UserChooseRecord;
+import com.lhjl.tzzs.proxy.utils.OwnerMapper;
+
+public interface UserChooseRecordMapper extends OwnerMapper<UserChooseRecord> {
+}
