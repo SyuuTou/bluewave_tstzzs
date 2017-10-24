cc1784fe4938773d0da1f2acd8376831d96e6a91
RoyLiuMingyan <liumingyan@idatavc.com>
2017-10-24 16:58:11 +0800

    认证审核java代码

---
+++ src/main/java/com/lhjl/tzzs/proxy/dto/TouZiDto.java
@@ -0,0 +1,58 @@
+package com.lhjl.tzzs.proxy.dto;
+
+import javax.management.loading.PrivateClassLoader;
+
+public class TouZiDto {
+
+	private String dateName;  //投资
+	private String compellation; // 姓名
+	private String	Organization;  // 所在公司
+	private String fillOffice;  //担任职务
+	private String evaContent;  //说明
+	private String tempFilePaths; //图片
+	private String mountID;  //认证id
+	public String getDateName() {
+		return dateName;
+	}
+	public void setDateName(String dateName) {
+		this.dateName = dateName;
+	}
+	public String getCompellation() {
+		return compellation;
+	}
+	public void setCompellation(String compellation) {
+		this.compellation = compellation;
+	}
+	public String getOrganization() {
+		return Organization;
+	}
+	public void setOrganization(String organization) {
+		Organization = organization;
+	}
+	public String getFillOffice() {
+		return fillOffice;
+	}
+	public void setFillOffice(String fillOffice) {
+		this.fillOffice = fillOffice;
+	}
+	public String getEvaContent() {
+		return evaContent;
+	}
+	public void setEvaContent(String evaContent) {
+		this.evaContent = evaContent;
+	}
+	public String getTempFilePaths() {
+		return tempFilePaths;
+	}
+	public void setTempFilePaths(String tempFilePaths) {
+		this.tempFilePaths = tempFilePaths;
+	}
+	public String getMountID() {
+		return mountID;
+	}
+	public void setMountID(String mountID) {
+		this.mountID = mountID;
+	}
+	
+
+}
