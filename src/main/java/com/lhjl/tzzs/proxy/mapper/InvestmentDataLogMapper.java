638e8902623727112fa7e08d5ba699348243bc7b
YangYangyy <670052206@qq.com>
2017-11-01 16:44:02 +0800

    investment_data_log表提交

---
+++ src/main/java/com/lhjl/tzzs/proxy/mapper/InvestmentDataLogMapper.java
@@ -0,0 +1,7 @@
+package com.lhjl.tzzs.proxy.mapper;
+
+import com.lhjl.tzzs.proxy.model.InvestmentDataLog;
+import com.lhjl.tzzs.proxy.utils.OwnerMapper;
+
+public interface InvestmentDataLogMapper extends OwnerMapper<InvestmentDataLog> {
+}
