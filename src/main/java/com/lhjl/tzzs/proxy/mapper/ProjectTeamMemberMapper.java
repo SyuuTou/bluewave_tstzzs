18adb79319289d1709c925de1cb871e4d7892dfa
RoyLiuMingyan <liumingyan@idatavc.com>
2017-11-08 09:52:45 +0800

    团队成员表

---
+++ src/main/java/com/lhjl/tzzs/proxy/mapper/ProjectTeamMemberMapper.java
@@ -0,0 +1,7 @@
+package com.lhjl.tzzs.proxy.mapper;
+
+import com.lhjl.tzzs.proxy.model.ProjectTeamMember;
+import com.lhjl.tzzs.proxy.utils.OwnerMapper;
+
+public interface ProjectTeamMemberMapper extends OwnerMapper<ProjectTeamMember> {
+}
