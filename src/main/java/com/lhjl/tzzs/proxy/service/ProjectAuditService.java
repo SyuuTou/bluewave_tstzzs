f6775ddc2021edcf4cb24fffb31b753c39b4398b
YangYangyy <670052206@qq.com>
2017-11-10 15:52:55 +0800

    删除无效引用

--- src/main/java/com/lhjl/tzzs/proxy/service/ProjectAuditService.java
+++ src/main/java/com/lhjl/tzzs/proxy/service/ProjectAuditService.java
@@ -2,11 +2,7 @@ package com.lhjl.tzzs.proxy.service;
 
 import com.lhjl.tzzs.proxy.dto.CommonDto;
 import com.lhjl.tzzs.proxy.dto.ProjectAuditInputDto;
-import org.springframework.stereotype.Service;
 
-import java.util.Map;
-
-@Service
 public interface ProjectAuditService {
     CommonDto<String> adminAuditProject(ProjectAuditInputDto body);
 }
