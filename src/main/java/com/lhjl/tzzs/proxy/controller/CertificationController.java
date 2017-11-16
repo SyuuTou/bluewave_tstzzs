f2f85dbd845e74cd69b6af976b6762a416e66554
RoyLiuMingyan <liumingyan@idatavc.com>
2017-11-16 21:01:59 +0800

    机构名称改成POST方式

--- src/main/java/com/lhjl/tzzs/proxy/controller/CertificationController.java
+++ src/main/java/com/lhjl/tzzs/proxy/controller/CertificationController.java
@@ -3,10 +3,13 @@ package com.lhjl.tzzs.proxy.controller;
 import java.util.List;
 import java.util.Map;
 
+import com.lhjl.tzzs.proxy.dto.InvestorsNameDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lhjl.tzzs.proxy.dto.CommonDto;
@@ -17,16 +20,23 @@ public class CertificationController {
 	private static Logger log = LoggerFactory.getLogger(CertificationController.class);
 	@Autowired
 	private CertificationService certificationService;
+
     /**
-     * 后台查询机构
-     * @param investorsName
+     * 查询机构接口
+     * @param body
      * @return
      */
-	@GetMapping("search/certification")
-    public CommonDto<List<Map<String, Object>>> findcertification(String investorsName){
+	@PostMapping ("search/certification")
+    public CommonDto<List<Map<String, Object>>> findcertification(@RequestBody  InvestorsNameDto body){
         CommonDto<List<Map<String, Object>>> result =new CommonDto<List<Map<String, Object>>>();
         try {
+            String investorsName = body.getInvestorsName();
+            if(investorsName !=null ){
             result = certificationService.findcertification(investorsName);
+            }else{
+                result.setStatus(5102);
+                result.setMessage("机构出现异常");
+            }
             if(result.getStatus() == null){
                 result.setStatus(200);
                 result.setMessage("success");
