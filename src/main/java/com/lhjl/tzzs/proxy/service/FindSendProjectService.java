daacc532f398917e41fa130e4613d6c382200119
RoyLiuMingyan <liumingyan@idatavc.com>
2017-11-19 17:48:03 +0800

    查找机构列表

---
+++ src/main/java/com/lhjl/tzzs/proxy/service/FindSendProjectService.java
@@ -0,0 +1,11 @@
+package com.lhjl.tzzs.proxy.service;
+
+import com.lhjl.tzzs.proxy.dto.CommonDto;
+import com.lhjl.tzzs.proxy.dto.ProjectSendListDto;
+
+import java.util.List;
+import java.util.Map;
+
+public interface FindSendProjectService {
+    CommonDto<List<Map<String,Object>>> findIntegralsSend(String shortName, Integer pageNum, Integer pageSize);
+}
