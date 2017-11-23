5621d0479774fbd114a288bf2eefc2da8b356588
YangYangyy <670052206@qq.com>
2017-11-23 22:19:01 +0800

    记录用户操作记录接口重写完成

--- src/main/java/com/lhjl/tzzs/proxy/controller/LogInfoController.java
+++ src/main/java/com/lhjl/tzzs/proxy/controller/LogInfoController.java
@@ -1,16 +1,22 @@
 package com.lhjl.tzzs.proxy.controller;
 
 import com.lhjl.tzzs.proxy.dto.CommonDto;
+import com.lhjl.tzzs.proxy.service.LogInfoService;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.util.Map;
 
 @RestController
 public class LogInfoController {
     private static final org.slf4j.Logger log = LoggerFactory.getLogger(LogInfoController.class);
+
+    @Resource
+    private LogInfoService logInfoService;
 
     @PostMapping("user/set/log")
     public CommonDto<String> userSetLog(@RequestBody Map<String,Object> body){
@@ -32,6 +38,28 @@ public class LogInfoController {
         result.setMessage("success");
         result.setStatus(200);
 
+        return result;
+    }
+
+    /**
+     * 记录用户操作记录的信息
+     * @param token 用户token
+     * @param data 对应数据
+     * @param sceneKey 场景
+     * @return
+     */
+    @GetMapping("user/save/log")
+    public CommonDto<String> userSaveLog(String token,String data,String sceneKey){
+        CommonDto<String> result = new CommonDto<>();
+        try {
+            result = logInfoService.saveUserInfo(token,data,sceneKey);
+        }catch (Exception e){
+            log.error(e.getMessage(),e.fillInStackTrace());
+
+            result.setStatus(502);
+            result.setMessage("服务器端发生错误");
+            result.setData(null);
+        }
         return result;
     }
 }
