a1c8204bc3802fd74edef999e8e5cef966a4be5a
YangYangyy <670052206@qq.com>
2017-11-24 10:18:55 +0800

    主体表提交

---
+++ src/main/java/com/lhjl/tzzs/proxy/mapper/SubjectMapper.java
@@ -0,0 +1,7 @@
+package com.lhjl.tzzs.proxy.mapper;
+
+import com.lhjl.tzzs.proxy.model.Subject;
+import com.lhjl.tzzs.proxy.utils.OwnerMapper;
+
+public interface SubjectMapper extends OwnerMapper<Subject> {
+}
