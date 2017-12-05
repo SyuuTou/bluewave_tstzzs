bef693a8de0f3926d0e7a0cf1da95171e3fae8d8
YangYangyy <670052206@qq.com>
2017-12-05 11:21:51 +0800

    提交机构地址表

---
+++ src/main/java/com/lhjl/tzzs/proxy/mapper/InvestmentInstitutionsAddressMapper.java
@@ -0,0 +1,7 @@
+package com.lhjl.tzzs.proxy.mapper;
+
+import com.lhjl.tzzs.proxy.model.InvestmentInstitutionsAddress;
+import com.lhjl.tzzs.proxy.utils.OwnerMapper;
+
+public interface InvestmentInstitutionsAddressMapper extends OwnerMapper<InvestmentInstitutionsAddress> {
+}
