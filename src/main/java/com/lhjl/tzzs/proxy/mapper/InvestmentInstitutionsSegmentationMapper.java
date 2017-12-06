a1835de94f5641326bbecdc5d06c880409a36e2d
YangYangyy <670052206@qq.com>
2017-12-06 14:45:24 +0800

    获取机构详情接口完成

--- src/main/java/com/lhjl/tzzs/proxy/mapper/InvestmentInstitutionsSegmentationMapper.java
+++ src/main/java/com/lhjl/tzzs/proxy/mapper/InvestmentInstitutionsSegmentationMapper.java
@@ -2,6 +2,17 @@ package com.lhjl.tzzs.proxy.mapper;
 
 import com.lhjl.tzzs.proxy.model.InvestmentInstitutionsSegmentation;
 import com.lhjl.tzzs.proxy.utils.OwnerMapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
 
 public interface InvestmentInstitutionsSegmentationMapper extends OwnerMapper<InvestmentInstitutionsSegmentation> {
+
+    /**
+     * 根据机构id获取机构领域信息
+     * @param institutionId
+     * @return
+     */
+    List<Map<String,Object>> findSegment(@Param("institutionId") Integer institutionId);
 }
