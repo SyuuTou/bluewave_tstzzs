1c018a1c23d9ad32eb3e34e3d5f29b8c55949f4f
YangYangyy <670052206@qq.com>
2017-12-12 15:54:25 +0800

    提交身份类型元数据表

---
+++ src/main/java/com/lhjl/tzzs/proxy/mapper/MetaIdentityTypeMapper.java
@@ -0,0 +1,7 @@
+package com.lhjl.tzzs.proxy.mapper;
+
+import com.lhjl.tzzs.proxy.model.MetaIdentityType;
+import com.lhjl.tzzs.proxy.utils.OwnerMapper;
+
+public interface MetaIdentityTypeMapper extends OwnerMapper<MetaIdentityType> {
+}
