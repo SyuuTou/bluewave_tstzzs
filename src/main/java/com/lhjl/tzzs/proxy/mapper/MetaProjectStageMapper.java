f732466dbfa86eb6c8e7c4d3f4e21e55d58289c5
YangYangyy <670052206@qq.com>
2017-12-15 18:44:49 +0800

    提交项目轮次元数据表

---
+++ src/main/java/com/lhjl/tzzs/proxy/mapper/MetaProjectStageMapper.java
@@ -0,0 +1,7 @@
+package com.lhjl.tzzs.proxy.mapper;
+
+import com.lhjl.tzzs.proxy.model.MetaProjectStage;
+import com.lhjl.tzzs.proxy.utils.OwnerMapper;
+
+public interface MetaProjectStageMapper extends OwnerMapper<MetaProjectStage> {
+}
