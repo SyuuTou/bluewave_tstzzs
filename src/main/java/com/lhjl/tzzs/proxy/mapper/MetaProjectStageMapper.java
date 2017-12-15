5763e9914639c57e040b1f15bcfe7b265c6598b9
YangYangyy <670052206@qq.com>
2017-12-15 21:05:32 +0800

    机构筛选接口提交

--- src/main/java/com/lhjl/tzzs/proxy/mapper/MetaProjectStageMapper.java
+++ src/main/java/com/lhjl/tzzs/proxy/mapper/MetaProjectStageMapper.java
@@ -2,6 +2,10 @@ package com.lhjl.tzzs.proxy.mapper;
 
 import com.lhjl.tzzs.proxy.model.MetaProjectStage;
 import com.lhjl.tzzs.proxy.utils.OwnerMapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface MetaProjectStageMapper extends OwnerMapper<MetaProjectStage> {
+    List<Integer> findMetaProjectStageByName(@Param("stages") String[] stages);
 }
--- src/main/java/com/lhjl/tzzs/proxy/mapper/MetaSegmentationMapper.java
+++ src/main/java/com/lhjl/tzzs/proxy/mapper/MetaSegmentationMapper.java
@@ -2,6 +2,10 @@ package com.lhjl.tzzs.proxy.mapper;
 
 import com.lhjl.tzzs.proxy.model.MetaSegmentation;
 import com.lhjl.tzzs.proxy.utils.OwnerMapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface MetaSegmentationMapper extends OwnerMapper<MetaSegmentation> {
+    List<Integer> findMetaSegmentationBySegmentation(@Param("domains") String[] domains);
 }
