dc9de83b1a57a2f759dd181ccdcdb8e4447f8627
YangYangyy <670052206@qq.com>
2017-12-15 21:08:21 +0800

    提交遗漏

--- src/main/java/com/lhjl/tzzs/proxy/mapper/ProjectsMapper.java
+++ src/main/java/com/lhjl/tzzs/proxy/mapper/ProjectsMapper.java
@@ -79,7 +79,7 @@ public interface ProjectsMapper extends OwnerMapper<Projects> {
      * @param stages
      * @return
      */
-    List<Integer>screenInvestmentAll(@Param("domains")String [] domains,@Param("stages")String [] stages);
+    List<Integer>screenInvestmentAll(@Param("domains")Integer[] domains,@Param("stages")Integer[] stages);
 
     /**
      * 获取用户创建项目编号最大值的接口
