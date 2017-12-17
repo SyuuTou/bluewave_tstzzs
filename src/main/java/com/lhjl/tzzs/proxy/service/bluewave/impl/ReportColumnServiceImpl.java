43460d5ee14b974b1f3565916ceff2307710cace
YangYangyy <670052206@qq.com>
2017-12-17 11:01:15 +0800

    添加注释

--- src/main/java/com/lhjl/tzzs/proxy/service/bluewave/impl/ReportColumnServiceImpl.java
+++ src/main/java/com/lhjl/tzzs/proxy/service/bluewave/impl/ReportColumnServiceImpl.java
@@ -1,9 +1,14 @@
 package com.lhjl.tzzs.proxy.service.bluewave.impl;
 
+import com.lhjl.tzzs.proxy.model.ReportColumn;
 import com.lhjl.tzzs.proxy.service.GenericService;
 import com.lhjl.tzzs.proxy.service.bluewave.ReportColumnService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReportColumnServiceImpl extends GenericService implements ReportColumnService {
+    @Override
+    public void save(ReportColumn metaColumn) {
+
+    }
 }
