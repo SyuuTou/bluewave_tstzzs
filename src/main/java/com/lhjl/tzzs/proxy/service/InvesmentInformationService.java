a6fbf2ef9d1a289aead765770cdd3a8d393fb0f1
YangYangyy <670052206@qq.com>
2017-12-19 18:06:48 +0800

    提交获取最近活跃机构的接口

--- src/main/java/com/lhjl/tzzs/proxy/service/InvesmentInformationService.java
+++ src/main/java/com/lhjl/tzzs/proxy/service/InvesmentInformationService.java
@@ -2,6 +2,7 @@ package com.lhjl.tzzs.proxy.service;
 
 import com.lhjl.tzzs.proxy.dto.CommonDto;
 import com.lhjl.tzzs.proxy.dto.InvestmentInstitutionsDto;
+import com.lhjl.tzzs.proxy.dto.SaveInformationDto;
 
 import java.util.List;
 
@@ -11,6 +12,13 @@ import java.util.List;
 public interface InvesmentInformationService {
     CommonDto<List<InvestmentInstitutionsDto>> findFiveInvestmentNew(Integer pageNum, Integer pageSize, String token);
     CommonDto<List<InvestmentInstitutionsDto>> findNotFiveInvestmentNew(Integer pageNum, Integer pageSize,String token);
-    CommonDto<List<InvestmentInstitutionsDto>> findRecommendCreater( String token);
-    CommonDto<List<InvestmentInstitutionsDto>>findRecommendInvestor( String token);
+    CommonDto<List<InvestmentInstitutionsDto>> findRecommendCreater(String token);
+    CommonDto<List<InvestmentInstitutionsDto>> findRecommendInvestor(String token);
+
+    /**
+     * 获取最近活跃的机构
+     * @param body
+     * @return
+     */
+    CommonDto<List<InvestmentInstitutionsDto>> findRecentlyInstitution(SaveInformationDto body);
 }
