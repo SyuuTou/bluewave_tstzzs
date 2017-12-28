804f248bd7c75b722559dadd5f17e1409a92e73b
YangYangyy <670052206@qq.com>
2017-12-28 17:47:13 +0800

    删除表

--- src/main/java/com/lhjl/tzzs/proxy/mapper/ProjectSendsMapper.java
+++
@@ -1,7 +0,0 @@
-package com.lhjl.tzzs.proxy.mapper;
-
-import com.lhjl.tzzs.proxy.model.ProjectSends;
-import com.lhjl.tzzs.proxy.utils.OwnerMapper;
-
-public interface ProjectSendsMapper extends OwnerMapper<ProjectSends> {
-}
