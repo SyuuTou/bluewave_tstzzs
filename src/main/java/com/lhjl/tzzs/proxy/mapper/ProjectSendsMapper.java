edc7291fea8d0a386a59c2049f870d5fc2bf0315
YangYangyy <670052206@qq.com>
2017-12-28 13:56:06 +0800

    提交审核项目表，拉出1.2.7分支

---
+++ src/main/java/com/lhjl/tzzs/proxy/mapper/ProjectSendsMapper.java
@@ -0,0 +1,7 @@
+package com.lhjl.tzzs.proxy.mapper;
+
+import com.lhjl.tzzs.proxy.model.ProjectSends;
+import com.lhjl.tzzs.proxy.utils.OwnerMapper;
+
+public interface ProjectSendsMapper extends OwnerMapper<ProjectSends> {
+}
