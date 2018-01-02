7e9a28d93b825b8b318afa9ca1311635c0a08364
YangYangyy <670052206@qq.com>
2018-01-02 10:26:12 +0800

    项目详情修改融资阶段排序，融资方信息读取

--- src/main/java/com/lhjl/tzzs/proxy/mapper/ProjectFinancingLogMapper.java
+++ src/main/java/com/lhjl/tzzs/proxy/mapper/ProjectFinancingLogMapper.java
@@ -1,11 +1,14 @@
 package com.lhjl.tzzs.proxy.mapper;
 
+import com.lhjl.tzzs.proxy.model.InvestmentInstitutions;
 import com.lhjl.tzzs.proxy.model.ProjectFinancingLog;
 import com.lhjl.tzzs.proxy.utils.OwnerMapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 public interface ProjectFinancingLogMapper extends OwnerMapper<ProjectFinancingLog> {
     List<ProjectFinancingLog> selectProjectFinancingLogList(@Param("projectId") Integer projectId,@Param("projectStatus") Integer projectStatus);
+    List<String> selectInvestors(@Param("pflid") Integer pflid);
 }
