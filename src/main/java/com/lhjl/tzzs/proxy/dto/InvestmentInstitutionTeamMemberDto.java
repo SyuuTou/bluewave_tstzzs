e32c114e554eb33cff4f23c51d7b38940173c3c7
YangYangyy <670052206@qq.com>
2018-01-05 17:38:49 +0800

    提交dto

---
+++ src/main/java/com/lhjl/tzzs/proxy/dto/InvestmentInstitutionTeamMemberDto.java
@@ -0,0 +1,36 @@
+package com.lhjl.tzzs.proxy.dto;
+
+public class InvestmentInstitutionTeamMemberDto {
+    /**高清图片*/
+    private String picture;
+
+    /**真实姓名*/
+    private String actualName;
+
+    /**担任职务*/
+    private String companyDuties;
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getActualName() {
+        return actualName;
+    }
+
+    public void setActualName(String actualName) {
+        this.actualName = actualName;
+    }
+
+    public String getCompanyDuties() {
+        return companyDuties;
+    }
+
+    public void setCompanyDuties(String companyDuties) {
+        this.companyDuties = companyDuties;
+    }
+}
