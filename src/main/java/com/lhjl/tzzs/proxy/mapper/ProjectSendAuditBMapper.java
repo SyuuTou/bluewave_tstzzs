62578fc080fdc033b33ceac838f51ad4e550aebd
YangYangyy <670052206@qq.com>
2018-01-13 18:01:04 +0800

    提交遗漏

--- src/main/java/com/lhjl/tzzs/proxy/mapper/ProjectSendAuditBMapper.java
+++ src/main/java/com/lhjl/tzzs/proxy/mapper/ProjectSendAuditBMapper.java
@@ -1,5 +1,7 @@
 package com.lhjl.tzzs.proxy.mapper;
 
+
+import com.lhjl.tzzs.proxy.dto.ProjectSendAuditBListDto;
 import com.lhjl.tzzs.proxy.model.ProjectSendAuditB;
 import com.lhjl.tzzs.proxy.utils.OwnerMapper;
 import org.apache.ibatis.annotations.Param;
@@ -8,4 +10,5 @@ import java.util.List;
 
 public interface ProjectSendAuditBMapper extends OwnerMapper<ProjectSendAuditB> {
     List<ProjectSendAuditB> searchProjectSendAuditB(@Param("userid") Integer userid,@Param("appid") Integer appid);
+    List<ProjectSendAuditBListDto> searchProjectSendAuditBList(@Param("shortName") String shortName,@Param("startPage") Integer startPage,@Param("pageSize") Integer pageSize ,@Param("appid") Integer appid);
 }
