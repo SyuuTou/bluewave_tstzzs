7ec9651cf73bc3e4f762690cf905e7c3c2739280
zhhu <huzhaohui@keepyoga.com>
2018-01-19 23:22:44 +0800

    新闻活动相关事件接口

---
+++ src/main/java/com/lhjl/tzzs/proxy/mapper/ReportCommentMapper.java
@@ -0,0 +1,7 @@
+package com.lhjl.tzzs.proxy.mapper;
+
+import com.lhjl.tzzs.proxy.model.ReportComment;
+import com.lhjl.tzzs.proxy.utils.OwnerMapper;
+
+public interface ReportCommentMapper extends OwnerMapper<ReportComment> {
+}
