2b2837803ae547f013b02f8ef90cd6edf1c5d46f
syuutousan <zhoudong@bluewavevc.com>
2018-01-31 13:49:16 +0800

    匹配用户智能搜索的接口编写

--- src/main/java/com/lhjl/tzzs/proxy/service/InvestorService.java
+++ src/main/java/com/lhjl/tzzs/proxy/service/InvestorService.java
@@ -18,7 +18,7 @@ public interface InvestorService {
 	/**
      * 投资人的用户匹配
      * @param appid
-     * @param keyword 搜索关键字
+     * @param keyword 搜索关键字  
      * @return
      */
 	CommonDto<List<Users>> matchUsers(Integer appid, String keyword);
