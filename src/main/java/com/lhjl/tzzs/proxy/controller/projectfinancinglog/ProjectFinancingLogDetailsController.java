8660e278afcabd93d1bbd3785214a58a472d6c8b
syuutousan <zhoudong@bluewavevc.com>
2018-02-07 11:19:02 +0800

    格式化代码

--- src/main/java/com/lhjl/tzzs/proxy/controller/projectfinancinglog/ProjectFinancingLogDetailsController.java
+++ src/main/java/com/lhjl/tzzs/proxy/controller/projectfinancinglog/ProjectFinancingLogDetailsController.java
@@ -10,15 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lhjl.tzzs.proxy.controller.GenericController;
-import com.lhjl.tzzs.proxy.dto.ChangePrincipalInputDto;
-import com.lhjl.tzzs.proxy.dto.CommonDto;
-import com.lhjl.tzzs.proxy.dto.investorDto.InvestorListInputDto;
-import com.lhjl.tzzs.proxy.model.Users;
-import com.lhjl.tzzs.proxy.service.InvestorService;
 import com.lhjl.tzzs.proxy.service.ProjectFinancingLogService;
 
 @RestController
