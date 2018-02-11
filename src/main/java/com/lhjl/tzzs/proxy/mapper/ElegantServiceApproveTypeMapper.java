e7e9675c62c0ff043d238f4d070a2ddfe8adcc27
syuutousan <zhoudong@bluewavevc.com>
2018-02-11 11:24:47 +0800

    修正了精选服务增加的bug

--- src/main/java/com/lhjl/tzzs/proxy/mapper/ElegantServiceApproveTypeMapper.java
+++ src/main/java/com/lhjl/tzzs/proxy/mapper/ElegantServiceApproveTypeMapper.java
@@ -1,7 +1,9 @@
 package com.lhjl.tzzs.proxy.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.lhjl.tzzs.proxy.model.ElegantServiceApproveType;
 import com.lhjl.tzzs.proxy.utils.OwnerMapper;
-
+@Mapper
 public interface ElegantServiceApproveTypeMapper extends OwnerMapper<ElegantServiceApproveType> {
 }
