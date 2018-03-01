ee4d56517d2892e53ac55b60ce7e7efa4ed54a69
syuutousan <zhoudong@bluewavevc.com>
2018-03-01 15:37:49 +0800

    更改 新增投资人时接口 时的有效标志位为1 有效

--- src/main/java/com/lhjl/tzzs/proxy/service/impl/InvestorInfoServiceImpl.java
+++ src/main/java/com/lhjl/tzzs/proxy/service/impl/InvestorInfoServiceImpl.java
@@ -74,6 +74,8 @@ public class InvestorInfoServiceImpl extends GenericService implements InvestorI
         investors.setSelfDefTeam(body.getSelfDefTeam());
         investors.setPhone(body.getPhone());
         investors.setKernelDescription(body.getKernelDesc());
+        //设置为有效数据 1
+        investors.setYn(1);
 //        Integer investorInsertResult = -1;
         //增加或者更新之后的投资人主键id
         Integer updateAfterId=0;
