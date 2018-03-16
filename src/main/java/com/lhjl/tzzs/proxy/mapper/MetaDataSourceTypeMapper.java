d18200b65179eed3098cd29e27691c2d2e9d0643
syuutousan <zhoudong@bluewavevc.com>
2018-03-16 17:42:34 +0800

    删除无用代码

--- src/main/java/com/lhjl/tzzs/proxy/mapper/MetaDataSourceTypeMapper.java
+++ src/main/java/com/lhjl/tzzs/proxy/mapper/MetaDataSourceTypeMapper.java
@@ -1,16 +1,7 @@
 package com.lhjl.tzzs.proxy.mapper;
 
-import java.util.List;
-
-import com.lhjl.tzzs.proxy.model.InvestorSegmentation;
 import com.lhjl.tzzs.proxy.model.MetaDataSourceType;
 import com.lhjl.tzzs.proxy.utils.OwnerMapper;
 
 public interface MetaDataSourceTypeMapper extends OwnerMapper<MetaDataSourceType> {
-	/**
-	 * 
-	 * @return
-	 */
-	List<MetaDataSourceType> getEntityTest();
-	  
 }
