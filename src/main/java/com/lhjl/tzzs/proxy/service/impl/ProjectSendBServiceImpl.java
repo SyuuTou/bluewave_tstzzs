c53a31d0134c56710a7d62fc2c530ef257d66174
syl950318 <shiyanling@bluewavevc.com>
2018-04-02 09:23:01 +0800

    天使投资指数提交项目列表

--- src/main/java/com/lhjl/tzzs/proxy/service/impl/ProjectSendBServiceImpl.java
+++ src/main/java/com/lhjl/tzzs/proxy/service/impl/ProjectSendBServiceImpl.java
@@ -1211,6 +1211,7 @@ public class ProjectSendBServiceImpl implements ProjectSendBService{
 
         PagingOutputDto<ProjectKernelInfoOutputDto> pagingOutputDto = new PagingOutputDto<>();
 
+
         if (body.getPageSize() == null){
             body.setPageSize(pageSizeDefault);
         }
